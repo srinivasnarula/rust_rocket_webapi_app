f1bd7242c7dc68131d7694b0cca4c8db3434a534
SrinivasNarula9 <srinivas.n27@gmail.com>
2022-11-30 13:54:45 +0000

    update hello world example

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,17 @@
 use rocket::serde::json::{Value, json};
 
 #[get("/")]
-fn hello() -> Value {
-    json!("Hello, World!")
+fn hello_json() -> Value {
+    json!({
+        "status": "error",
+        "reason": "Resource was not found."
+    })
 }
 
 #[rocket::main]
 async fn main() {
     let _= rocket::build()
-        .mount("/", routes![hello])
+        .mount("/", routes![hello_json])
         .launch()
         .await;
 }
