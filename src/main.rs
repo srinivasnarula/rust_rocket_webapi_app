3e93efb70b7be60f90725b2d730651c82d941f03
SrinivasNarula9 <srinivas.n27@gmail.com>
2022-11-30 23:36:52 +0000

    Added Error Catch Handler

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,30 @@ use rocket::serde::json::{Value, json};
 #[get("/")]
 fn hello_json() -> Value {
     json!({
-        "status": "error",
-        "reason": "Resource was not found."
+        "status": "204",
+        "reason": "Success with no content"
     })
 }
+
+#[get("/employee/<_id>")]
+fn get_employee(_id : i32) -> Value {
+    json!({
+        "Name": "Srinivas",
+        "Title": "Rust Architect"
+    })
+}
+
+#[catch(404)]
+fn not_found() -> Value {
+    json!({ "status": "Not found!"})
+}
+
 
 #[rocket::main]
 async fn main() {
     let _= rocket::build()
-        .mount("/", routes![hello_json])
+        .mount("/", routes![hello_json,get_employee])
+        .register("/", catchers![not_found])
         .launch()
         .await;
 }
